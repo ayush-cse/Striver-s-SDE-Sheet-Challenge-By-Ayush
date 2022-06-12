7a21e6d66597ed85f77ed655400e2f12d5378d2c
Ayush Kumar Bhardawaj <60316666+ayush-cse@users.noreply.github.com>
2022-06-12 06:59:15 +0530

    Add files via upload

---
+++ DAY2Challenge5_MergeInterval.cpp
@@ -0,0 +1,45 @@
+#include<algorithm>
+#include<bits/stdc++.h>
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+
+{
+
+   // Write your code here.
+
+   vector<vector<int> > mergeAns;
+
+ sort(intervals.begin(),intervals.end());
+
+   vector<int> pr=intervals[0];
+
+   for(int i=1;i<intervals.size();i++)
+
+   {
+
+       if(intervals[i][0]<=pr[1])
+
+       {
+
+           pr[1]=max(pr[1],intervals[i][1]);
+
+       }
+
+       else
+
+       {
+
+           mergeAns.push_back(pr);
+
+            pr=intervals[i];
+
+       }
+
+   }
+
+   mergeAns.push_back(pr);
+
+ 
+
+   return mergeAns;
+
+}
